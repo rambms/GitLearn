527882068ba0eac6b43eceeb83eaa767e644b608
rambms <rambms07@gmail.com>
2020-07-26 18:20:35 +0530

    Commiting to new changes

--- TestProject/src/test/core/pkg/Test.java
+++ TestProject/src/test/core/pkg/Test.java
@@ -4,7 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Ram Kumar");
-		System.out.println("This is my notes");
+		System.out.println("This is my notess");
+		System.out.println("This is one more message");
 
 	}
 
